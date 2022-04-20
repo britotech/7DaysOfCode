b97473aa421c2ed87df7a7e0efdf0b69f3cb4b13
Anderson Brito Garcia <andersonbritogarcia@gmail.com>
2022-04-20 18:19:45 -0300

    Dia  - Criacao do record filmes para exibicao

--- 7DaysOfCode/src/tech/brito/sevendaysofcode/Main.java
+++ 7DaysOfCode/src/tech/brito/sevendaysofcode/Main.java
@@ -9,7 +9,6 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 import java.util.Scanner;
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
@@ -24,9 +23,8 @@ public class Main {
             var json = obterJsonResposta(apiKey);
             validarInexistenciaErroResposta(json);
 
-            var filmes = obterFilmes(json);
-            filmes.forEach(f -> System.out.println(f.getTitle()));
-            filmes.forEach(f -> System.out.println(f.getImage()));
+            var movies = obterFilmes(json);
+            movies.forEach(System.out::println);
         } catch (Exception ex) {
             System.out.println("Falha na obtenção da lista de filmes. " + ex.getMessage());
         }
@@ -71,118 +69,24 @@ public class Main {
     }
 
     private static String extrairConteudo(String texto, String regex) {
-
-        var pattern = Pattern.compile(regex, Pattern.CASE_INSENSITIVE);
-        Matcher comparator = pattern.matcher(texto);
-        if (comparator.find(0)) {
-            return comparator.group("meuGrupo");
-        }
-
-        return "";
+        var matcher = Pattern.compile(regex, Pattern.CASE_INSENSITIVE).matcher(texto);
+        return matcher.find(0) ? matcher.group("meuGrupo") : "";
     }
 
-    private static List<Filme> obterFilmes(String json) {
+    private static List<Movie> obterFilmes(String json) {
         var items = extrairConteudo(json, "\\[(?<meuGrupo>.*?)\\]");
-        var retorno = items.split("\\}");
 
-        return Arrays.stream(retorno).map(item -> {
-            var filme = new Filme();
-            filme.setId(extrairConteudo(item, "\\\"id\":\"(?<meuGrupo>.*?)\\\""));
-            filme.setRank(extrairConteudo(item, "\\\"rank\":\"(?<meuGrupo>.*?)\\\""));
-            filme.setTitle(extrairConteudo(item, "\\\"title\":\"(?<meuGrupo>.*?)\\\""));
-            filme.setFullTitle(extrairConteudo(item, "\\\"fullTitle\":\"(?<meuGrupo>.*?)\\\""));
-            filme.setYear(extrairConteudo(item, "\\\"year\":\"(?<meuGrupo>.*?)\\\""));
-            filme.setImage(extrairConteudo(item, "\\\"image\":\"(?<meuGrupo>.*?)\\\""));
-            filme.setCrew(extrairConteudo(item, "\\\"crew\":\"(?<meuGrupo>.*?)\\\""));
-            filme.setImDbRating(extrairConteudo(item, "\\\"imDbRating\":\"(?<meuGrupo>.*?)\\\""));
-            filme.setImDbRatingCount(extrairConteudo(item, "\\\"imDbRatingCount\":\"(?<meuGrupo>.*?)\\\""));
-            return filme;
-        }).collect(Collectors.toList());
+        return Arrays
+                .stream(items.split("\\}"))
+                .map(item -> new Movie(extrairConteudo(item, "\\\"id\":\"(?<meuGrupo>.*?)\\\""),
+                                       extrairConteudo(item, "\\\"rank\":\"(?<meuGrupo>.*?)\\\""),
+                                       extrairConteudo(item, "\\\"title\":\"(?<meuGrupo>.*?)\\\""),
+                                       extrairConteudo(item, "\\\"fullTitle\":\"(?<meuGrupo>.*?)\\\""),
+                                       extrairConteudo(item, "\\\"year\":\"(?<meuGrupo>.*?)\\\""),
+                                       extrairConteudo(item, "\\\"image\":\"(?<meuGrupo>.*?)\\\""),
+                                       extrairConteudo(item, "\\\"crew\":\"(?<meuGrupo>.*?)\\\""),
+                                       extrairConteudo(item, "\\\"imDbRating\":\"(?<meuGrupo>.*?)\\\""),
+                                       extrairConteudo(item, "\\\"imDbRatingCount\":\"(?<meuGrupo>.*?)\\\"")))
+                .collect(Collectors.toList());
     }
 }
-
-class Filme {
-
-    private String id;
-    private String rank;
-    private String title;
-    private String fullTitle;
-    private String year;
-    private String image;
-    private String crew;
-    private String imDbRating;
-    private String imDbRatingCount;
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getRank() {
-        return rank;
-    }
-
-    public void setRank(String rank) {
-        this.rank = rank;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getFullTitle() {
-        return fullTitle;
-    }
-
-    public void setFullTitle(String fullTitle) {
-        this.fullTitle = fullTitle;
-    }
-
-    public String getYear() {
-        return year;
-    }
-
-    public void setYear(String year) {
-        this.year = year;
-    }
-
-    public String getImage() {
-        return image;
-    }
-
-    public void setImage(String image) {
-        this.image = image;
-    }
-
-    public String getCrew() {
-        return crew;
-    }
-
-    public void setCrew(String crew) {
-        this.crew = crew;
-    }
-
-    public String getImDbRating() {
-        return imDbRating;
-    }
-
-    public void setImDbRating(String imDbRating) {
-        this.imDbRating = imDbRating;
-    }
-
-    public String getImDbRatingCount() {
-        return imDbRatingCount;
-    }
-
-    public void setImDbRatingCount(String imDbRatingCount) {
-        this.imDbRatingCount = imDbRatingCount;
-    }
-}
-
---
+++ 7DaysOfCode/src/tech/brito/sevendaysofcode/Movie.java
@@ -0,0 +1,13 @@
+package tech.brito.sevendaysofcode;
+
+public record Movie(String id,
+                    String rank,
+                    String title,
+                    String fullTitle,
+                    String year,
+                    String image,
+                    String crew,
+                    String imDbRating,
+                    String imDbRatingCount) {
+
+}
